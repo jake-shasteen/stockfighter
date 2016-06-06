b1ff21aba396c9645ecb08f6b7384f326f629877
Jake Shasteen <jakeshasteen@gmail.com>
2016-06-06 03:05:11 -0400

    Increment with proper key

--- chock_a_block.py
+++ chock_a_block.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 import datetime, os, requests, time
 
-account = raw_input( 'What is your account?' )
-venue = raw_input( 'What is the venue?' )
-stock = raw_input( 'What is the stock?' )
-total = raw_input( 'How much do you want to buy total?' )
-price = raw_input( 'What is your maximum buy price?' )
+account = raw_input( 'What is your account? ' )
+venue = raw_input( 'What is the venue? ' )
+stock = raw_input( 'What is the stock? ' )
+total = raw_input( 'How much do you want to buy total? ' )
+price = int(raw_input( 'What is your maximum buy price? ' ))
 
 headers = { 'X-Starfighter-Authorization': os.environ.get('STARFIGHTER_API_KEY') }
 
 new_order = 'https://api.stockfighter.io/ob/api/venues/' + venue + '/stocks/' + stock + '/orders'
 
-body = {'account': account, 'venue': venue, 'stock': stock, 'price': price, 'direction': 'buy', 'orderType': 'immediate-or-fill'}
-body['qty'] = 100
+body = {'account': account, 'venue': venue, 'stock': stock, 'price': price, 'direction': 'buy', 'orderType': 'ioc'}
+body['qty'] = 1000
 
 print 'START'
 ts = time.time()
@@ -20,10 +20,10 @@ print datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 bought = 0
 while bought < total:
-  response = requests.post( new_order, data=body, headers=headers )
-  bought += response.totalFilled
+  response = requests.post( new_order, json=body, headers=headers )
+  bought += response.json()[u'totalFilled']
   print bought
-  time.sleep(5)
+  time.sleep(1)
 
 print 'END'
 ts = time.time()
